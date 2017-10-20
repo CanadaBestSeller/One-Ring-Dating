d60389447f75f86174c8e17bbe6aaced827e2086
Li <lidavid@amazon.com>
2017-10-20 16:40:08 -0700

    Folders + other nice stuff

--- modules/face_extractor/face_extractor_server.py
+++ modules/face_extractor/face_extractor_server.py
@@ -12,6 +12,7 @@ import cv2
 
 # Configs
 RESULT_FOLDER = "phase_1_selection/"
+ESSENTIAL_FOLDERS = [RESULT_FOLDER + NAME for NAME in ['ids/', 'faces/', 'edges/', 'originals/']]
 CASCADE_PATH = os.path.abspath("modules/face_extractor/haarcascade_frontalface_default.xml")
 RESULT_EXTENSION = ".jpg"
 FACE_SIZE = (100, 100)
@@ -52,23 +53,29 @@ class FaceExtractorServer(socketserver.BaseRequestHandler):
 
     @staticmethod
     def process(face_extraction_request):
+        FaceExtractorServer.ensure_face_folders_exist()
         platform = face_extraction_request['platform']
         handle = face_extraction_request['handle']
         image_links = face_extraction_request['image_links']
 
         for index, image_link in enumerate(image_links):
-            image_filepath = FaceExtractorServer.save_image(platform, handle, image_link, index)
-            FaceExtractor.extract_face(image_filepath)
+            image_folder_path, image_filename = FaceExtractorServer.save_image(platform, handle, image_link, index)
+            FaceExtractor.extract_face(image_folder_path, image_filename)
 
     @staticmethod
     def save_image(platform, handle, image_link, index):
-        extension = os.path.splitext(image_link)[1]
-        open(RESULT_FOLDER + platform + '-' + handle + '.id', 'a').close()
-        image_filename = str(index+1) + '---' + platform + '-' + handle + extension
-        image_filepath = RESULT_FOLDER + 'original-' + image_filename
-        urlretrieve(image_link, image_filepath)
-        logging.info("Image has been successfully downloaded and saved as {0}".format(image_filepath))
-        return image_filepath
+        open(RESULT_FOLDER + 'ids/' + platform + '-' + handle + '.id', 'a').close()
+        image_filename = platform + '-' + handle + '-' + str(index+1) + RESULT_EXTENSION
+        image_folder_path = RESULT_FOLDER + 'originals/'
+        urlretrieve(image_link, image_folder_path + image_filename)
+        logging.info("Image has been successfully downloaded and saved as {0}".format(image_folder_path))
+        return image_folder_path, image_filename
+
+    @staticmethod
+    def ensure_face_folders_exist():
+        for folder_path in ESSENTIAL_FOLDERS:
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
 
 
 class FaceExtractionRequest(dict):
@@ -85,14 +92,14 @@ class FaceExtractionRequest(dict):
 
 class FaceExtractor:
     @staticmethod
-    def extract_face(image_filepath):
-        logging.info("Processing {0}".format(image_filepath))
+    def extract_face(image_folder_path, image_filename):
+        logging.info("Processing [{1}] @ {0}".format(image_folder_path, image_filename))
 
         # Create the Haar cascade
         face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
 
         # Read the image
-        image = cv2.imread(image_filepath)
+        image = cv2.imread(image_folder_path + image_filename)
         resize_ratio = RESIZE_TOP_PIXELS/image.shape[1]
         image = cv2.resize(image, (0, 0,), fx=resize_ratio, fy=resize_ratio)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -104,12 +111,12 @@ class FaceExtractor:
             minNeighbors=20,
             minSize=(30, 30),
         )
-        logging.info("Processing {0}. Faces found: {1}.".format(image_filepath, len(faces)))
+        logging.info("Faces found: {0}.".format(len(faces)))
 
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        annotated_filename = os.path.dirname(image_filepath) + '/edge-' + os.path.basename(image_filepath)
+        annotated_filename = image_folder_path + '../edges/' + image_filename
         cv2.imwrite(annotated_filename, image)
         logging.info("Saving face-annotated image as {0}".format(annotated_filename))
 
@@ -118,13 +125,15 @@ class FaceExtractor:
             for (x, y, w, h) in faces:
                 face_grayscale = gray[y:y+h, x:x+w]
                 face_grayscale_resized = cv2.resize(face_grayscale, FACE_SIZE)
-                face_filename = os.path.dirname(image_filepath) + '/face-' + os.path.basename(image_filepath)
+                face_filename = image_folder_path + '../faces/' + image_filename
                 cv2.imwrite(face_filename, face_grayscale_resized)
-                logging.info("Saving extracted grayscale face as {0}".format(face_filename))
+                logging.info("FOUND 1 FACE! Saving extracted grayscale face as {0}".format(face_filename))
         elif len(faces) == 0:
             logging.info("Found no faces. Not extracting any faces")
         else:
             logging.info("Found multiple faces. Not extracting any faces")
+
+        logging.info("Face extraction finished.\n")
 
 
 def initialize_logger():
@@ -151,8 +160,18 @@ if __name__ == '__main__':
     serving_host, serving_port = sys.argv[1], int(sys.argv[2])
     initialize_logger()
 
-    logging.info("Serving requests @ {}:{}".format(serving_host, serving_port))
-    while True:
-        server = socketserver.TCPServer((serving_host, serving_port), FaceExtractorServer)
-        server.request_queue_size = 5
+    logging.info("\n"
+                 "\n-----------------------------------"
+                 "\nFace Extraction Server online!"
+                 "\nServing requests @ {}:{}"
+                 "\n-----------------------------------"
+                 "\n".format(serving_host, serving_port))
+
+    server = socketserver.TCPServer((serving_host, serving_port), FaceExtractorServer)
+    server.request_queue_size = 5
+    try:
         server.serve_forever()
+    except KeyboardInterrupt:
+        logging.info('Shutting down server...')
+        server.shutdown()
+        logging.info('Done!\n')
